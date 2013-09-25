3d50b045a7094e3c257e5096b00af38dbd577e30
Ariel Ekgren <ariel.ekgren@gmail.com>
2013-09-25 16:43:21 +0200

    started animating

--- solutionEvaluator/SolutionEvaluator.java
+++ solutionEvaluator/SolutionEvaluator.java
@@ -20,6 +20,7 @@ import java.util.Vector;
 public class SolutionEvaluator {
 	private Sokoban path;
 	private String filePath;
+	private Vector<StringBuilder> board = new Vector<StringBuilder>();
 
 	public SolutionEvaluator() throws IOException{
 		System.out.println("Initializing evaluation... \n");
@@ -35,32 +36,59 @@ public class SolutionEvaluator {
 	}
 	
 	public void animateSolution() throws FileNotFoundException, IOException{
-		Vector<StringBuilder> board = new Vector<StringBuilder>();
-
+		
+		
 		BufferedReader fileBr = new BufferedReader(new FileReader(filePath));
 		
-		StringBuilder line = null;
+		String line = null;
+		boolean STATE = false;
+		
+		int[] xy = new int[2];
+		int[] player = new int[2];
 		
 		while(fileBr.ready()) {
-			line = new StringBuilder(fileBr.readLine());
-			board.add(line);
+			line = fileBr.readLine();
+			if(STATE == false){
+                xy[0] = line.indexOf("+");
+                if ( xy[0] != -1 ) {
+                	player[0] = xy[0];
+                    player[1] = xy[1];
+                    STATE = true;
+                }
+        
+                xy[0] = line.indexOf("@");
+                if ( xy[0] != -1 ) {
+                        player[0] = xy[0];
+                        player[1] = xy[1];
+                        STATE = true;
+                }
+                xy[1] += 1;
+			}
+            System.out.println(board.size());
+			board.add(new StringBuilder(line));
 		} // End while
-		fileBr.close();
-
+		//fileBr.close();
 		
+		//board.elementAt(processNode.y).setCharAt(processNode.x, 'O');
+		
+		for (int i = 0; i < path.solution.length(); i++){
+		    char c = path.solution.charAt(i);
+		    System.out.println(c);
+		    printMap();
+			}
 		
 	}
 	
-    /*public void printMap(){
+    public void printMap(){
         for(StringBuilder s : board){
                 System.out.println(s);
         }
         try {
-            Thread.sleep(50);
+            Thread.sleep(100);
         } catch(InterruptedException ex) {
             Thread.currentThread().interrupt();
         }
-    }*/
+    }
 	
 	public static void main(String[] args) throws IOException{
 		SolutionEvaluator MapEval = new SolutionEvaluator();
