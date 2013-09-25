af506c4cf7a1cf1c2d21654b487d44b8415a9c75
Hjerpe <hjerpe.adam@gmail.com>
2013-09-25 14:34:28 +0200

    getStringToGoal is now working

--- sokoban/Sokoban.java
+++ sokoban/Sokoban.java
@@ -37,7 +37,20 @@ public class Sokoban {
 		 Solver solver = new Solver(); //Reaches info incl. initial state from Map staticaly.
 		 State finalState = solver.getFinalState();
 		 
-		// System.out.println(solver.getStrToGoal(finalState));
+		 System.out.println(solver.getStrToGoal(finalState));
+		 Visualizer.printState(board.getInitialState(), "INITIAL STATE");
+		 
+		 while(finalState.getParent() != null){
+			 System.out.println(finalState.getCharLastMove());
+			 System.out.println(finalState.getLastMovedBox().getRow());
+			 System.out.println(finalState.getLastMovedBox().getCol());
+			 System.out.println(finalState.getPlayerRow());
+			 System.out.println(finalState.getPlayerCol());
+			 finalState = finalState.getParent();
+			 
+		 }
+		 System.out.println(finalState.getPlayerRow());
+		 System.out.println(finalState.getPlayerCol());
 		 
 		 if (debugMode){
 			 //System.out.println(solver.solutionPath());
