d0d91cad585b32ea9c1918160c38eef31f01dcfa
Ariel Ekgren <ariel.ekgren@gmail.com>
2013-10-09 18:54:59 +0200

    added player

---
+++ sokoban/Player.java
@@ -0,0 +1,14 @@
+package sokoban;
+
+import java.awt.Point;
+
+/**
+ * NEO-SOKOBAN PLAYER CLASS.
+ * 
+ * Extends the java Point class.
+ */
+
+@SuppressWarnings("serial")
+public class Player extends Point {
+
+}
