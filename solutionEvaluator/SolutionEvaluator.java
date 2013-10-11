dc0a5e448eac2e4b0da140de02a810f8109b538b
Ariel Ekgren <ariel.ekgren@gmail.com>
2013-10-11 14:32:33 +0200

    small changes

--- solutionEvaluator/SolutionEvaluator.java
+++ solutionEvaluator/SolutionEvaluator.java
@@ -16,7 +16,7 @@ public class SolutionEvaluator {
 
     // Insert Level number (0 = TestTrack) 6401, 6027
 
-    private int levelNo = 6035;
+    private int levelNo = 21;
     private MapReader map;
 
 	public SolutionEvaluator() throws IOException{
