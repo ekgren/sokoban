0568edf9c8fbd757b28617a390957802218da4c3
Hjerpe <hjerpe.adam@gmail.com>
2013-10-11 12:24:01 +0200

    changed map

--- solutionEvaluator/SolutionEvaluator.java
+++ solutionEvaluator/SolutionEvaluator.java
@@ -18,7 +18,7 @@ public class SolutionEvaluator {
 	private Vector<StringBuilder> board = new Vector<StringBuilder>();
 
     // Insert Level number (0 = TestTrack)
-    private int levelNo = 6030;
+    private int levelNo = 0;
     private MapReader map;
 
 	public SolutionEvaluator() throws IOException{
